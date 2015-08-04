672c39baf0a73290f6a6a969981ce4252df94de4
Eric Andrew Lewis <eric.andrew.lewis@gmail.com>
2015-08-04 03:16:08 -0400

    Add main.cpp

---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include "simple_Polygon.cpp"
+
+int main(int argc, const char * argv[]) {
+    Polygon *test = new Polygon(3);
+    test->V->x = 4;
+    test->V->y = 5;
+    // how should I set the other vertices
+
+    return 0;
+}
